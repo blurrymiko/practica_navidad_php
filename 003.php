55bc59955044e0ac414f7d1e909a9562a27cc21e
rodrigo <rodrigoalvarezg@outlook.com>
2026-01-07 20:36:20 +0100

    version_0.0

---
+++ 003.php
@@ -0,0 +1,7 @@
+<?php
+declare(strict_types=1);
+
+// EJERCICIO 3: Analizador de texto
+
+// Introducir del texto
+$texto = "PHP no está muerto… solo sigue trabajando silenciosamente en el 80% de Internet";
