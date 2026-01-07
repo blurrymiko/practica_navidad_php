d9af742c5e5d6d83fee0ce5002035396412dcbfc
rodrigo <rodrigoalvarezg@outlook.com>
2026-01-07 19:28:20 +0100

    add calificacion
    Creada la función calificación para definir si aprueban o suspenden

--- 001.php
+++ 001.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 // EJERCICIO 1: Gestor de notas de estudiantes
 
 // 1. Introducir datos de entrada. 
-// La estructura será $estudiantes - $nombre => $notas
+# La estructura será $estudiantes - $nombre => $notas
 $estudiantes = [
     "Ana" => [8, 7, 9],
     "Luis" => [5, 6, 4],
@@ -22,7 +22,27 @@ function calcularPromedio(array $notas): float
     return array_sum($notas) / count($notas);
 }
 
+// 3. Variables a mostrar ($aprobados $suspensos $mejorEstudiante $mejorPromedio)
+# Se colocan fuera porque si no se reiniciarían en cada recorrido.el mejor promedio a -1 para que cualquier promedio real (>=0) lo supere.
+$aprobados = 0;
+$suspensos = 0;
+
+$mejorEstudiante = "";
+$mejorPromedio = -1.0;
 
 
 // Recorrer los datos de entrada
-//foreach ($estudiantes as $nombre => $notas)
+foreach ($estudiantes as $nombre => $notas) {
+
+  # Calcular el promedio con la función calcularPromedio
+  $promedio = calcularPromedio($notas);
+
+  # Definir si aprueban o suspenden en función del promedio (>=6 $aprobado)
+  $calificacion = ($promedio >= 6) ? "Aprobado" : "Suspenso";
+  if ($calificacion === "Aprobado") {
+    $aprobados++;
+  } else {
+    $suspensos++;
+    }
+
+}
