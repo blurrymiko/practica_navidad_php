f85588e06427859c313d444f38b6b180e615ba50
rodrigo <blurrymiko@outlook.com>
2026-01-08 16:18:38 +0100

    version_0.2
    añadidas funciones cadenas

--- 003.php
+++ 003.php
@@ -5,3 +5,13 @@ declare(strict_types=1);
 
 // Introducir del texto
 $texto = "PHP no está muerto… solo sigue trabajando silenciosamente en el 80% de Internet";
+
+// Cambiar al cadena de caracteres a minúsculas y limpiar caracteres especiales.
+// • strtolower y strtoupper: Transforman una cadena de caracteres en la misma cadena en minúsculas o mayúsculas respectivamente. • str_replace: reemplaza caracteres en una cadena
+
+$minusculas = mb_strtolower($texto)
+$textoFinal = str_replace('/[^\p{L}\p{N}]+/u', ' ', $minusculas);
+
+// Si queremos romper las cadenas en trozos, tenemos: • explode: convierte en array la cadena mediante un separador.
+
+
