e822553db8a5639d491126e3a2bdb09b791a898e
rodrigo <blurrymiko@outlook.com>
2026-01-08 16:27:06 +0100

    version_0.3
    añadido recorrido y conteo de ´numero de palabras

--- 003.php
+++ 003.php
@@ -6,12 +6,27 @@ declare(strict_types=1);
 // Introducir del texto
 $texto = "PHP no está muerto… solo sigue trabajando silenciosamente en el 80% de Internet";
 
-// Cambiar al cadena de caracteres a minúsculas y limpiar caracteres especiales.
-// • strtolower y strtoupper: Transforman una cadena de caracteres en la misma cadena en minúsculas o mayúsculas respectivamente. • str_replace: reemplaza caracteres en una cadena
+//1. Cambiar al cadena de caracteres a minúsculas y limpiar caracteres especiales.
 
 $minusculas = mb_strtolower($texto)
 $textoFinal = str_replace('/[^\p{L}\p{N}]+/u', ' ', $minusculas);
 
 // Si queremos romper las cadenas en trozos, tenemos: • explode: convierte en array la cadena mediante un separador.
 
+$trozos = ($textoFinal === '') ? [] : explode(' ', $textoFinal);
+
+// 6. Recorrer cada palabra e ignorar palabras con menos de tres letras
+
+$palabras = [];
+
+foreach ($trozos as $trozos) {
+    if (mb_strlen($trozos, 'UTF-8') >= 3) {
+        $palabras[] = $trozos;
+    }
+}
+
+// 2. Contamos el número de palabras
+$totalPalabras = count($palabras);
+
+// 3. 
 
